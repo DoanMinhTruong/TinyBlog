a65432bf532273219dd4fd16c878013cb1bcf2d2
DoanMinhTruong <meir.truong.2309@gmail.com>
2022-07-03 05:06:40 +0700

    07/03/22

--- server/settings.py
+++ server/settings.py
@@ -125,13 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
+import django_heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
